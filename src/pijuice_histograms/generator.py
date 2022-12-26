cc9364cbff2fecda5ba1b8aa40892533e55bdea4
Bystroushaak <bystrousak@kitakitsune.org>
2022-12-26 19:34:38 +0100

    This version can actually generate graphs into files.

--- src/pijuice_histograms/generator.py
+++ src/pijuice_histograms/generator.py
@@ -1,44 +1,80 @@
-import time
+import os
 from datetime import datetime
+from datetime import timedelta
 
+import tqdm
 from matplotlib import pyplot
 from matplotlib.dates import DateFormatter
 
 from pijuice_histograms.orm import Storage
 
 
-def _print_all_values(path: str):
-    storage = Storage(path)
-    for datapoint in storage.get_datapoints_between(*_get_timestamps_for_today()):
-        print(datapoint)
+def generate_webpage_for_last_month(sqlite_path: str, report_path: str):
+    storage = Storage(sqlite_path)
+    report_path = os.path.join(report_path, "solarpi_report/")
 
+    today = datetime.today()
+    ranges = [today]
+    for x in range(30):
+        ranges.append(today - timedelta(days = x + 1))
 
-def generate_graph_for_today(storage: Storage):
+    os.makedirs(report_path, exist_ok=True)
+
+    report = open(os.path.join(report_path, "report.html"), "wt")
+    report.write("""<!DOCTYPE html>
+<html>
+<head>
+  <title>Bystroushaak's blog</title>
+</head>
+<body>
+<h1>SolarPi report</h1>
+""")
+
+    for cnt, date in tqdm.tqdm(list(enumerate(ranges))):
+        report.write(f"<h2>{date.strftime('%Y-%m-%d')}</h2>\n")
+        report.write(f"<img src='{cnt}.png' />\n")
+
+        generate_graph_for(storage, date, os.path.join(report_path, f"{cnt}.png"))
+
+    report.write("""</body>
+</html>""")
+    report.close()
+
+def generate_graph_for(storage: Storage, day=None, path=None):
+    if day is None:
+        day = datetime.now()
+
     timestamps = []
     status = []
-    for datapoint in storage.get_datapoints_between(*_get_timestamps_for_today()):
+    for datapoint in storage.get_datapoints_between(*_get_timestamps_for(day)):
         timestamps.append(datetime.fromtimestamp(datapoint.timestamp))
         status.append(datapoint.power_type.value)
 
-    pyplot.title(f"SolarPi status for {time.strftime('%Y-%m-%d')}")
+    pyplot.title(f"SolarPi status for {day.strftime('%Y-%m-%d')}")
     pyplot.xlabel("Time")
     pyplot.ylabel("Solar status")
     pyplot.axhline(y=3, color="b", linestyle=":", label="Present")
     pyplot.axhline(y=2, color="orange", linestyle=":", label="Weak")
     pyplot.axhline(y=1, color="r", linestyle=":", label="Bad or not present")
     pyplot.legend(loc = 'best')
-    pyplot.xlim(*_get_datetimes_for_today())
+    pyplot.xlim(*_get_datetimes_for(day, day))
 
     date_fmt = DateFormatter("%H:%M:%S")
     pyplot.gca().xaxis.set_major_formatter(date_fmt)
 
     pyplot.bar(timestamps, status)
     pyplot.gcf().autofmt_xdate()
-    pyplot.show()
+
+    if path is None:
+        pyplot.show()
+    else:
+        pyplot.gcf().set_size_inches(10, 4)
+        pyplot.savefig(path, dpi=150)
+        pyplot.close()
 
 
-def _get_timestamps_for_today():
-    start, end = _get_datetimes_for_today()
+def _get_timestamps_for(day):
+    start, end = _get_datetimes_for(day, day)
 
     start_ts = datetime.timestamp(start)
     end_ts = datetime.timestamp(end)
@@ -46,18 +82,8 @@ def _get_timestamps_for_today():
     return (start_ts, end_ts)
 
 
-def _get_datetimes_for_today():
-    start = datetime.now()
+def _get_datetimes_for(start, end):
     start = start.replace(hour=0, minute=0, second=0, microsecond=0)
-
-    end = datetime.now()
     end = end.replace(hour=23, minute=59, second=59, microsecond=0)
 
     return start, end
-
-
-if __name__ == "__main__":
-    storage = Storage("/home/bystrousak/Desktop/c0d3z/python/tools/pijuice_histograms/data.sqlite")
-    generate_graph_for_today(
-        storage
-    )
